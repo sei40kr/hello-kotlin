fefa0bd26df2eb7c566acb6735447487d21348e2
Seong Yong-ju <sei40kr@gmail.com>
2020-05-06 10:34:23 +0900

    chore: add collections/PlusAndMinusOperators

---
+++ src/main/kotlin/me/yong_ju/hello_kotlin/collections/PlusAndMinusOperators.kt
@@ -0,0 +1,9 @@
+package me.yong_ju.hello_kotlin.collections
+
+fun main() {
+    val numbers = listOf("one", "two", "three", "four")
+    val plusList = numbers + "five"
+    val minusList = numbers - listOf("three", "four")
+    println(plusList)
+    println(minusList)
+}
