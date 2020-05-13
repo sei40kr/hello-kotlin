934be27eeee1a6e673ef3fcaf26002f778fd971d
Seong Yong-ju <sei40kr@gmail.com>
2020-05-14 00:12:43 +0900

    chore: add coroutines/CancellationAndTimeouts

---
+++ src/main/kotlin/me/yong_ju/hello_kotlin/coroutines/CancellationAndTimeouts.kt
@@ -0,0 +1,138 @@
+package me.yong_ju.hello_kotlin.coroutines
+
+import kotlinx.coroutines.*
+
+fun main() {
+    //
+    // Cancelling coroutine execution
+    runBlocking {
+        val job = launch {
+            repeat(1000) { i ->
+                println("job: I'm sleeping $i ...")
+                delay(500L)
+            }
+        }
+        delay(1300L) // delay a bit
+        println("main: I'm tired of waiting!")
+        job.cancel() // cancels the job
+        job.join() // waits for job's completion
+        println("main: Now I can quit.")
+    }
+
+
+    //
+    // Cancellation is cooperative
+
+    runBlocking {
+        val startTime = System.currentTimeMillis()
+        val job = launch(Dispatchers.Default) {
+            var nextPrintTime = startTime
+            var i = 0
+            while (i < 5) { // computation loop, just wastes CPU
+                // print a message twice a second
+                if (System.currentTimeMillis() >= nextPrintTime) {
+                    println("job: I'm sleeping ${i++} ...")
+                    nextPrintTime += 500L
+                }
+            }
+        }
+        delay(1300L)
+        println("main: I'm tired of waiting!")
+        job.cancelAndJoin() // cancels the job and waits for its completion
+        println("main: Now I can quit.")
+    }
+
+
+    //
+    // Making computation code cancellable
+
+    runBlocking {
+        val startTime = System.currentTimeMillis()
+        val job = launch(Dispatchers.Default) {
+            var nextPrintTime = startTime
+            var i = 0
+            while (isActive) { // cancellable computation loop
+                // print a message twice a second
+                if (System.currentTimeMillis() >= nextPrintTime) {
+                    println("job: I'm sleeping ${i++} ...")
+                    nextPrintTime += 500L
+                }
+            }
+        }
+        delay(1300L)
+        println("main: I'm tired of waiting!")
+        job.cancelAndJoin() // cancels the job and waits for its completion
+        println("main: Now I can quit.")
+    }
+
+
+    //
+    // Closing resources with finally
+
+    runBlocking {
+        val job = launch {
+            try {
+                repeat(1000) { i ->
+                    println("job: I'm sleeping $i ...")
+                    delay(500L)
+                }
+            } finally {
+                println("job: I'm running finally")
+            }
+        }
+        delay(1300L)
+        println("main: I'm tired of waiting!")
+        job.cancelAndJoin() // cancels the job and waits for its completion
+        println("main: Now I can quit.")
+    }
+
+
+    //
+    // Run non-cancellable block
+
+    runBlocking {
+        val job = launch {
+            try {
+                repeat(1000) { i ->
+                    println("job: I'm sleeping $i ...")
+                    delay(500L)
+                }
+            } finally {
+                withContext(NonCancellable) {
+                    println("job: I'm running finally")
+                    delay(1000L)
+                    println("job: And I've just delayed for 1 sec because I'm non-cancellable")
+                }
+            }
+        }
+        delay(1300L)
+        println("main: I'm tired of waiting!")
+        job.cancelAndJoin() // cancels the job and waits for its completion
+        println("main: Now I can quit.")
+    }
+
+
+    //
+    // Timeout
+
+    runBlocking {
+        withTimeout(1300L) {
+            repeat(1000) { i ->
+                println("I'm sleeping $i ...")
+                delay(500L)
+            }
+        }
+    }
+
+    runBlocking {
+        val result = withTimeoutOrNull(1300L) {
+            repeat(1000) { i ->
+                println("I'm sleeping $i ...")
+                delay(500L)
+            }
+            "Done" // will get cancelled before it produces this result
+        }
+        // Result is null
+        println("Result is $result")
+    }
+}
